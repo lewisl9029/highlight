89b088e7e44a359db6e88f20794d7e31db9bf3c0
Lewis Liu <lewisl9029@users.noreply.github.com>
2023-04-05 16:04:09 -0700

    Update generated

--- frontend/src/__generated/ve/pages/LogsPage/LogsHistogram/LogsHistogram.css.js
+++ frontend/src/__generated/ve/pages/LogsPage/LogsHistogram/LogsHistogram.css.js
@@ -1,1 +1,1 @@
-var o="_155k3sw4",r="_155k3sw0",e="_155k3sw1",t="_155k3sw2",p="_155k3sw3";export{o as dragSelection,r as histogramContainer,e as hover,t as popoverContent,p as popoverContentRow};
+var r=120,e=2,o="4.16rem",t="_155k3sw3",p="_155k3sw0",v="_155k3sw1",a="_155k3sw2",_="_155k3sw4";export{r as OUTLINE_HISTOGRAM_HEIGHT,e as OUTLINE_PADDING,o as REGULAR_HISTOGRAM_HEIGHT,t as dragSelection,p as hover,v as popoverContent,a as popoverContentRow,_ as thresholdArea};
