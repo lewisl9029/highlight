7f6d8cf68025cc388c6bef33c3ad7a397b88ec05
Lewis Liu <lewisl9029@users.noreply.github.com>
2024-10-18 17:42:23 -0700

    Update

--- frontend/src/pages/Auth/SignIn.tsx
+++ frontend/src/pages/Auth/SignIn.tsx
@@ -142,7 +142,7 @@ export const SignIn: React.FC<Props> = ({ setResolver }) => {
 						<Heading level="h4">
 							{workspaceInvite
 								? `You're invited to join ‘${workspaceInvite.workspace_name}’`
-								: '!Welcome back.'}
+								: 'Welcome back!1234'}
 						</Heading>
 						{AUTH_MODE !== 'oauth' ? (
 							<Text>
